72aa7b70693cad7290ebc21451242172aaeb0dc4
ahmed mostafa <ahmed.moahmed442@gmail.com>
2024-02-08 23:20:14 +0200

    Add Loger And Postman Collection extraction

--- server.ts
+++ server.ts
@@ -1,26 +1,25 @@
 import express, { ErrorRequestHandler } from 'express';
 import asyncHandler from 'express-async-handler';
 import './datastore/datastore';
-
 import userRouter from './routers/users';
 import photosRouter from './routers/photos';
 import { errorHandler } from './middlewares/errorMiddleware.';
 import dorenv from 'dotenv';
 import { authMiddleware } from './middlewares/authMiddleware';
-// const upload = multer({ dest: 'uploads/' });
+import { loggerMiddleware } from './middlewares/logger';
 
 const main = async () => {
   dorenv.config();
   const app = express();
   app.use(express.json());
+  app.use(loggerMiddleware);
 
   app.use('/user', userRouter);
 
   app.use('/photos', asyncHandler(authMiddleware), photosRouter);
 
   app.use(errorHandler);
-
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server is running on Port: ${PORT}`);
   });
